2f5b0083f8672e6de29537f22687d7c106b88d5a
Rudra Patel <patelrudra219@gmail.com>
2022-09-23 22:26:13 +0530

    checks if a string is a palindrome or not

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+
+my_str = input("Enter string : ")
+
+# reverse the string
+rev_str = reversed(my_str)
+
+# check if the string is equal to its reverse
+if list(my_str) == list(rev_str):
+   print("The string is a palindrome.")
+else:
+   print("The string is not a palindrome.")
